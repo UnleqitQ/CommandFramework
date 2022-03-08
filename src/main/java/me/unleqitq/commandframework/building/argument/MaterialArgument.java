7ba89ede417b9d13bf5abed2bacea59946ff18ce
UnleqitQ <quentin.weinbrenner@gmail.com>
2022-03-08 19:10:33 +0100

    Update

--- src/main/java/me/unleqitq/commandframework/building/argument/MaterialArgument.java
+++ src/main/java/me/unleqitq/commandframework/building/argument/MaterialArgument.java
@@ -1,9 +1,11 @@
 package me.unleqitq.commandframework.building.argument;
 
+import me.unleqitq.commandframework.ICommandContext;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class MaterialArgument extends FrameworkArgument<Material> {
 	
@@ -22,9 +24,7 @@ public class MaterialArgument extends FrameworkArgument<Material> {
 	public static class Builder extends FrameworkArgument.Builder<Material> {
 		
 		public Builder(String name) {
-			super(name, (c, a) -> Material.matchMaterial(a),
-					(c, a) -> Arrays.stream(Material.values()).map(Material::getKey).map(NamespacedKey::getKey).filter(
-							s -> s.toLowerCase().startsWith(a.toLowerCase())).toList());
+			super(name, new MaterialParser(), new MaterialTabComplete(true, true));
 		}
 		
 		public Builder setDescription(String description) {
@@ -45,6 +45,43 @@ public class MaterialArgument extends FrameworkArgument<Material> {
 			builder.tabCompleteProvider = this.tabCompleteProvider;
 			builder.description = this.description;
 			return builder;
+		}
+		
+	}
+	
+	public static class MaterialParser implements Parser<Material> {
+		
+		@Override
+		public Material parse(ICommandContext context, String argument) {
+			for (Material material : Material.values()) {
+				if (material.getKey().getKey().replaceAll("_", "").equalsIgnoreCase(argument.replaceAll("_", "")))
+					return material;
+			}
+			return null;
+		}
+		
+	}
+	
+	public static class MaterialTabComplete implements TabCompleteProvider {
+		
+		boolean nonItems;
+		boolean nonBlocks;
+		
+		public MaterialTabComplete(boolean nonItems, boolean nonBlocks) {
+			this.nonItems = nonItems;
+			this.nonBlocks = nonBlocks;
+		}
+		
+		@Override
+		public List<String> tabComplete(ICommandContext context, String currentArgument) {
+			return Arrays.stream(Material.values()).filter(
+					m -> (nonItems || m.isItem()) && (nonBlocks || m.isBlock())).filter(m -> {
+				String n = m.getKey().getKey();
+				String[] ns = n.split("_");
+				String[] args = currentArgument.split("_");
+				return Arrays.stream(args).allMatch(a -> Arrays.stream(ns).anyMatch(
+						n0 -> n0.toLowerCase().replaceAll("_", "").startsWith(n0.toLowerCase().replaceAll("_", ""))));
+			}).map(Material::getKey).map(NamespacedKey::getKey).toList();
 		}
 		
 	}
